45633d7677b2f68ff0a059a053632d923174057f
migregal <devmigregal@mail.ru>
2024-10-04 16:48:59 +0300

    lab-02: added

---
+++ lab-02/src/graph/node.rs
@@ -0,0 +1,20 @@
+use derive_more::Constructor;
+
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Hash, Eq, Constructor, Debug)]
+pub struct Node {
+    number: i32,
+}
+
+impl Node {
+    pub fn number(&self) -> i32 {
+        self.number
+    }
+}
+
+impl fmt::Display for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Node({})", self.number())
+    }
+}
